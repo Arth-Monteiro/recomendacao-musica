8d5b30ccd64b37bdf25af0b013e0e52052770ca6
Arth-Monteiro <arthur.m.monteiro2902@gmail.com>
2020-10-27 15:51:31 -0300

    Redefinindo classes e tabelas do banco de dados

--- src/main/java/br/usjt/classes/User.java
+++ src/main/java/br/usjt/classes/User.java
@@ -1,26 +1,96 @@
 package br.usjt.classes;
 
+import br.usjt.dao.ConnectionFactory;
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import br.usjt.dao.ConnectionFactory;
+
 
 public class User {
-    
+    private String username, password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static boolean login(Connection conn, String username, String password) throws SQLException {
+        String query = "SELECT * FROM users WHERE username = ? AND senha = ?"; // Query para verificar se existe usuario
+        PreparedStatement stmt = conn.prepareStatement(query); // Prepara a Query
+
+        // Define variaveis na query
+        stmt.setString(1, username);
+        stmt.setString(2, password);
+
+        // Executa a Query
+        try (ResultSet res = stmt.executeQuery()) {
+            if (res.next()) {
+                return true; // Caso encontre algum usuario
+            } else {
+                return false; // Caso nao encontre usuario
+            }
+        }
+    }
+
+    public static String signin(Connection conn, String username, String password) throws SQLException {
+        String query = "SELECT * FROM users WHERE username = ?"; // Query para verificar se existe nome de usuario
+        PreparedStatement stmt = conn.prepareStatement(query); // Prepara a Query
+        stmt.setString(1, username);
+        try (ResultSet res = stmt.executeQuery()) {
+            if (res.next()) {
+                return "Nome de usuario já existente!";
+            } else {
+                String queryInsert = "INSERT INTO users (username, senha) VALUES (?,?)";
+                PreparedStatement stmtInsert = conn.prepareStatement(queryInsert);
+                stmtInsert.setString(1, username);
+                stmtInsert.setString(2, password);
+                stmtInsert.execute();
+                return "Usuario cadastrado!";
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Connection conn = ConnectionFactory.obterConexao();
         try {
-            Statement stmt = conn.createStatement();
-            String query = "SELECT * FROM login";
-            ResultSet res = stmt.executeQuery(query);
-            while (res.next()) {
-                String hash = res.getString(1);
-                System.out.println(hash);
-            }
-            conn.close();
+            System.out.println(signin(conn, "testedois", "ApenasUmTeste"));
+            System.out.println(login(conn, "testedois", "ApenasUmTeste"));
         } catch (SQLException e) {
-            System.out.println(e);
-        }        
+            System.out.println(e.getMessage());
+        }    
     }
 }
+
+// Apenas como documentacao para continuar o desenvolvimento
+// try {
+//     Statement stmt = conn.createStatement();
+//     String query = "SELECT * FROM login  WHERE username = 'TESTE'";
+//     ResultSet resultSet = stmt.executeQuery(query);
+//     ResultSetMetaData rsmd = resultSet.getMetaData();
+//     int columnsNumber = rsmd.getColumnCount();
+//     System.out.println(columnsNumber);
+//     while (resultSet.next()) {
+//         for (int i = 1; i <= columnsNumber; i++) {
+//             if (i > 1) System.out.print(",  ");
+//             String columnValue = resultSet.getString(i);
+//             System.out.print(columnValue + " " + rsmd.getColumnName(i));
+//         }   
+//         System.out.println("");
+//     } 
+//     conn.close();
+// } catch (SQLException e) {
+//     System.out.println(e.getMessage());
+// }    
--- tables.sql
+++ tables.sql
@@ -4,8 +4,8 @@ CREATE DATABASE projeto_recomendacao;
 
 USE projeto_recomendacao;
 
-CREATE TABLE login (
-login_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE users (
+user_id INT PRIMARY KEY AUTO_INCREMENT,
 username VARCHAR(20) NOT NULL UNIQUE,
 senha VARCHAR(16)
 );
@@ -25,9 +25,9 @@ FOREIGN KEY (genero_id) REFERENCES genero(genero_id)
 
 CREATE TABLE avaliacoes (
 avaliacoes_id INT PRIMARY KEY AUTO_INCREMENT,
-login_id INT,
+user_id INT,
 musica_id INT,
 nota_avaliacao INT NOT NULL,
-FOREIGN KEY (login_id) REFERENCES login(login_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id),
 FOREIGN KEY (musica_id) REFERENCES musica(musica_id)
 )
